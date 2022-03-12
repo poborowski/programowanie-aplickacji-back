5c2ab5903ce41e2906efba0bd3518313f485ac21
bartosz.poborowski <bpoborowski@gmail.com>
2022-03-12 17:54:36 +0100

    fd

--- src/com/company/Main2.java
+++ src/com/company/Main2.java
@@ -2,38 +2,31 @@ package com.company;
 
 public class Main2 {
 
-    int[]CRC_TABLE = Array(256);
 
-    private static int main(String[] args) {
 
-        for (let i = 0; i < 256; ++i) {
-            int code = i;
-            for (int j = 0; j < 8; ++j) {
-                code = (code & 0x01 ? 0xEDB88320 ^ (code >>> 1) : (code >>> 1));
+        public static void main(String[] args) {
+            System.out.println(crc32("This is example text ..."));  // 3473739588
+        };
+
+        static int[] CRC_TABLE = new int [256];
+        static {
+            for (int i = 0; i < 256; ++i) {
+                int code = i;
+                for (int j = 0; j < 8; ++j) {
+                    code = (code & 0x01) !=0 ? 0xEDB88320 ^ (code >>> 1) : (code >>> 1);
+                }
+                CRC_TABLE[i] = code;
             }
-            CRC_TABLE[i] = code;
         }
-    };
-private static int crc32(String text){
-    int crc = -1;
-    for (int i = 0; i < text.length; ++i) {
-        int code = text.charCodeAt(i);
-        crc = CRC_TABLE[(code ^ crc) & 0xFF] ^ (crc >>> 8);
-    }
-    return (-1 ^ crc) >>> 0;
-};
-
-//int crc32 = text => {
-//        int crc = -1;
-//        for (int i = 0; i < text.length; ++i) {
-//      	int code = text.charCodeAt(i);
-//            crc = CRC_TABLE[(code ^ crc) & 0xFF] ^ (crc >>> 8);
-//        }
-//        return (-1 ^ crc) >>> 0;
-//    };
+        public static int crc32(String text) {
+            int crc = -1;
+            for (int i = 0; i < text.length(); ++i) {
+                int code = text.charAt(i);
+                crc = CRC_TABLE[(code ^ crc) & 0xFF] ^ (crc >>> 8);
+            }
+            return (-1 ^ crc) >>> 0;
+        };
 
 
-
-     System.out.println(crc32('This is example text ...'));  // 3473739588
 
 }
